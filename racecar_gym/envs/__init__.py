63c18000c725ab1ab399ec8e9e3cee425e9e2ea5
Yulun Zhuang <z1079931505@gmail.com>
2023-03-13 17:38:23 -0400

    Update __init__.py

--- racecar_gym/envs/__init__.py
+++ racecar_gym/envs/__init__.py
@@ -1,5 +1,7 @@
 from . import gym_api
 from . import pettingzoo_api
 from .scenarios import MultiAgentScenario, SingleAgentScenario
+from .gym_api.changing_track import ChangingTrackMultiAgentRaceEnv
+from .gym_api.multi_agent_race import MultiAgentRaceEnv
 
-__all__ = ["pettingzoo_api", "gym_api", "MultiAgentScenario", "SingleAgentScenario"]
+__all__ = ["pettingzoo_api", "gym_api", "MultiAgentScenario", "SingleAgentScenario", "ChangingTrackMultiAgentRaceEnv"]
