76e5d4b70d2fb8493899e3178e08c1cbb0fb083b
Yulun Zhuang <z1079931505@gmail.com>
2023-03-13 16:34:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="racecar_gym",
     version="0.1",
     packages=find_packages(),
-    install_requires=['pybullet==3.1.7',
+    install_requires=[#'pybullet==3.1.7',
                       'scipy', # version automatically choosen w.r.t numpy
-                      'numpy==1.22.3',
-                      'gym==0.21.0',
+#                       'numpy==1.22.3',
+#                       'gym==0.21.0',
                       'yamldataclassconfig==1.5.0',
                       'nptyping<2.0',
                       'pettingzoo==1.18.1'
